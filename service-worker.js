29e34f0ec0b75d5cf965509fe741da3569e24bad
Zbyszek <57629429+ziggy83pl@users.noreply.github.com>
2025-03-07 21:44:24 +0100

    Add files via upload
    v1.7

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+// service-worker.js
+const CACHE_NAME = 'prodom-cache-v1';
+const urlsToCache = [
+'/',
+'/index.html',
+'/style.css',
+'/script.js',
+'/logo/icon-192x192.png',
+'/logo/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then((cache) => cache.addAll(urlsToCache))
+);
+});
+
+self.addEventListener('fetch', (event) => {
+event.respondWith(
+    caches.match(event.request)
+    .then((response) => response || fetch(event.request))
+);
+});
