04e14c57d844595e9acad94aa177a14d7d169068
Zbyszek <57629429+ziggy83pl@users.noreply.github.com>
2026-03-01 18:36:56 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // service-worker.js
-const CACHE_NAME = 'prodom-cache-v12';
+const CACHE_NAME = 'prodom-cache-v14';
 const urlsToCache = [
 './',
 './index.html',
@@ -58,6 +58,7 @@ self.addEventListener('fetch', (event) => {
             })
     );
 });
+
 
 
 
